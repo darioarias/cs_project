27dbe0f5f98fb3b576c09d302444ac98d9f8d212
Dario <darioarias12@gmail.com>
2022-03-01 15:02:24 -0500

    gitignore

---
+++ src/component/React/SinglyLinkedList.jsx
@@ -0,0 +1,102 @@
+import { default as SinglyLinkedList } from "../DSA/DataStructures/LinkedList/singlyLinkedList.js";
+import react from "react";
+import NodeFormat from "./SingleNode.jsx";
+
+class SinglyLinkedListComponent extends react.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+      list: null,
+    };
+
+    this.getHead = this.getHead.bind(this);
+    this.onChangeValue = this.onChangeValue.bind(this);
+  }
+
+  componentDidMount() {
+    this.resetList();
+  }
+
+  resetList() {
+    sa;
+    let newlist = new SinglyLinkedList();
+    this.setState({ list: newlist });
+  }
+
+  push() {
+    this.state.list.push(Number(this.state.value));
+    this.forceUpdate();
+  }
+
+  remove() {
+    this.state.list.remove(this.state.value);
+    this.forceUpdate();
+  }
+
+  pop() {
+    this.state.list.pop();
+    this.forceUpdate();
+  }
+
+  removeLast() {
+    this.state.list.removeLast();
+    this.forceUpdate();
+  }
+
+  getHead() {
+    if (this.state.list != null) {
+      return this.state.list.head;
+    }
+  }
+
+  onChangeValue(data) {
+    this.setState({ value: data.target.value });
+  }
+
+  render() {
+    let Nodes = () => {
+      let values = [];
+      if (this.state.list !== null) {
+        let headptr = this.getHead();
+        while (headptr != null) {
+          values.push(headptr);
+          headptr = headptr.next;
+        }
+        return (
+          <div>
+            {values.map((node) => (
+              <NodeFormat key={Math.random()} data={node}></NodeFormat>
+            ))}
+          </div>
+        );
+      } else {
+        return;
+      }
+    };
+    return (
+      <div>
+        <button onClick={() => this.resetList()}>Reset List</button>
+        <div>
+          <input value={this.state.value} onChange={this.onChangeValue} />
+          <button onClick={() => this.push()}> Push Value </button>
+          <button onClick={() => this.remove()}> Remove Value</button>
+          <button onClick={() => this.pop()}> Pop Value</button>
+          <button onClick={() => this.removeLast()}> Remove Last</button>
+        </div>
+
+        <div
+          className="jsavlist jsavautoresize jsavhorizontallist"
+          data-visible="true"
+          data-nodegap="40"
+          data-autoresize="true"
+        >
+          {Nodes()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SinglyLinkedListComponent;
