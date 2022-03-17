9e3a99dfec84f0ba4862372539035f741d7165b1
YasmeenY <90418738+YasmeenY@users.noreply.github.com>
2022-03-16 22:07:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Nav from "./component/webpages/Navbar";
 import { DataStructures, Algorithms } from "./component/DSA/exports";
-import Home from "./component/webpages/homepage";
+import Home from "./component/webpages/Home";
 
 // const LS = DataStructures.LinkedLists.SinglyLinkedList
 
@@ -59,18 +59,15 @@ class App extends Component {
     avl.print();
     console.log(avl);
     */
-    return (
-      <div>
-        <Nav />
-        {/* <div className="container-fluid">
-          <SinglyLinkedListComponent></SinglyLinkedListComponent>
-        </div> */}
-        <div>
-          <Home />
-        </div>
-      </div>
-    );
-  }
+   return (
+     <div>
+       <Nav />
+       <div>
+         <Home />
+       </div>
+     </div>
+   );
+ }
 }
 
 export default App;
