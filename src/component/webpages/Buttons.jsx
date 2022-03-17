e0e8ddec7c10d7735f89af48a58a2a00124df3e9
YasmeenY <90418738+YasmeenY@users.noreply.github.com>
2022-03-16 22:03:28 -0400

    Create Buttons.jsx

---
+++ src/component/webpages/Buttons.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+ 
+const Buttons = ({setDataStructure, setShowButtons}) => {
+ return (
+   <div>  
+     <button
+       style={{
+         marginTop: 50,
+         marginLeft: 50,
+         height: 50,
+         width: 100,
+         boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+         marginBottom: 50,
+         transition: "0.3s",
+       }}
+       onClick={() => {
+         setDataStructure("linkedList")
+         setShowButtons(false)
+       }}
+     >
+       Linked List
+     </button>
+     <button
+       animated="ease"
+       style={{
+         marginTop: 50,
+         marginLeft: 50,
+         height: 50,
+         width: 100,
+         boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+         marginBottom: 50,
+         transition: "0.3s",
+       }}
+       onClick={() => {
+         setDataStructure("stack")
+         setShowButtons(false)
+       }}
+     >
+       Stack
+     </button>
+     <button
+       animated="ease"
+       style={{
+         marginTop: 50,
+         marginLeft: 50,
+         height: 50,
+         width: 100,
+         boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+         marginBottom: 50,
+         transition: "0.3s",
+       }}
+       onClick={() => {
+         setDataStructure("queue")
+         setShowButtons(false)
+       }}
+     >
+       Queue
+     </button>
+     <button
+       animated="ease"
+       style={{
+         marginTop: 50,
+         marginLeft: 50,
+         height: 50,
+         width: 110,
+         boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+         marginBottom: 50,
+         transition: "0.3s",
+       }}
+       onClick={() => {
+         setDataStructure("binaryTree")
+         setShowButtons(false)
+       }}
+     >
+       Binary Trees
+     </button>
+   </div>
+ );
+};
+ 
+export default Buttons;
