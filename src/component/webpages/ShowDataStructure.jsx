39e1f6034c8624aee1ee61d32f3d61a40add66d1
unknown <james.yoo715@gmail.com>
2022-03-18 10:13:42 -0400

    changed some files to show my stack

--- src/component/webpages/ShowDataStructure.jsx
+++ src/component/webpages/ShowDataStructure.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import SinglyLinkedListComponent from "../React/SinglyLinkedList";
 import BinaryTreeComponent from "../React/BinaryTree";
 import QueueComponent from "../React/Queue";
+import StackComponent from "../React/Stack";
  
 const ShowDataStructure = ({dataStructure}) => {
  return (
    <div>
        {dataStructure === "linkedList" && <SinglyLinkedListComponent/>}
-       {dataStructure === "stack" && "Stacks"}
+       {dataStructure === "stack" && <StackComponent/>}
        {dataStructure === "binaryTree" && <BinaryTreeComponent/>}
        {dataStructure === "queue" && <QueueComponent/>}
    </div>
