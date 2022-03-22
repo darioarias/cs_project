540952a0e6db83fa4d678faab9d345c1b3517e03
James Yoo <44252946+jamyooes@users.noreply.github.com>
2022-03-22 14:35:40 -0400

    Update Dark.jsx

--- src/component/webpages/Dark.jsx
+++ src/component/webpages/Dark.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 
 class Dark extends React.Component {
-  render() {
-    return (
-      <div className="theme-switch-wrapper">
-        <label className="theme-switch" htmlFor="checkbox">
-          <input type="checkbox" id="checkbox" />
-          <div className="slider round"></div>
-        </label>
-        <em>Enable Dark Mode!</em>
-      </div>
-    );
-  }
+    constructor(props){
+      super(props);
+      this.state = {
+        backgrounds: "",
+      };
+    }
+
+    componentDidMount() {
+      this.setState({backgrounds:"lightMode"}, ()=>this.setColors());
+    }
+
+    setColors(){
+      if (this.state.backgrounds === "lightMode"){
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+      }
+      else{
+        document.body.style.backgroundColor = "#566573";
+        document.body.style.color = "#f8f9f9";      
+      }
+    }
+
+    assignClass(){
+      if (this.state.backgrounds === "lightMode"){
+        this.setState({backgrounds:"darkMode"}, ()=>this.setColors());
+      }
+      else{
+        this.setState({backgrounds:"lightMode"}, ()=>this.setColors());
+      }
+    }
+
+    render(){
+        return(  
+        <div class="theme-switch-wrapper">
+          <label class="theme-switch" for="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <div class="slider round" onClick={()=>this.assignClass()}></div>
+          </label>
+        </div>
+      );
+    }
 }
 
-export default Dark;
+export default Dark
