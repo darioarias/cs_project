fbed64bbfa98eb51a890a0b7de925918c827196f
Azaan <xxx.azaan.ali@gmail.com>
2022-04-07 23:42:33 -0400

    sandbox update

--- src/component/sandbox/sandbox.jsx
+++ src/component/sandbox/sandbox.jsx
@@ -1,9 +1,46 @@
-import react from 'react'
+import React, { useState, useEffect } from "react";
+import PathfindingViz from '../visualizer/PathFindingViz/Pathfind'
+import LinkedListComponent from "../visualizer/SinglyLinkedList";
+import BinaryTreeComponent from "../visualizer/BinaryTree";
+import QueueComponent from "../visualizer/Queue";
+import StackComponent from "../visualizer/Stack";
+import HeapComponent from "../visualizer/Heap";
 
 const Sandbox = () => {
+  const [option, setOption] = useState("PathFinding");
+
+  const handleOptionSelect = (e) => {
+    setOption(e.target.value);
+  };
+
+  const renderSwitch = () =>{
+    switch(option) {
+      case "PathFinding":
+        return (<PathfindingViz></PathfindingViz>);
+      case "LinkedList":
+        return (<LinkedListComponent></LinkedListComponent>);
+      case "Queue":
+        return (<QueueComponent></QueueComponent>);
+      case "Stack":
+        return(<StackComponent></StackComponent>);
+      case "Heap":
+        return(<HeapComponent></HeapComponent>);
+    }
+  }
+
  return (
    <div>
-     Sandbox
+     <h2>Sandbox</h2>
+     <select value={option} onChange={handleOptionSelect}>
+          <option value="PathFinding">PathFinding</option>
+          <option value="LinkedList">LinkedList</option>
+          <option value="Queue">Queue</option>
+          <option value="Stack">Stack</option>
+          <option value="Heap">Heap</option>
+      </select>
+     <div>
+       {renderSwitch()}
+     </div>
    </div>
  );
 };
