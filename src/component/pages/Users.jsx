ee863be04e9142ee5b12f371874b55e46ca7b628
unknown <james.yoo715@gmail.com>
2022-04-18 17:56:41 -0400

    something weird happened to azaan's commit

---
+++ src/component/pages/Users.jsx
@@ -0,0 +1,14 @@
+/*
+This is just for testing purposes to check reflect the users for the get requests
+*/
+import React, { useState } from "react";
+
+const Users = () =>{
+    return (
+        <div>
+            YEP
+        </div>
+    );
+};
+
+export default Users;
