d0b8a115c1d289f234750eadad22257a102f501e
unknown <james.yoo715@gmail.com>
2022-04-26 20:56:46 -0400

    set up get requests for test pages

--- src/component/pages/UserProfile.jsx
+++ src/component/pages/UserProfile.jsx
@@ -1,21 +1,37 @@
 /*
 This is just for testing purposes to check reflect the users for the get requests
 */
+import axios from 'axios';
 import React, { Component, useState } from "react";
 
 class UserProfile extends Component {
     constructor(props){
         super(props)
         this.state = {
-            posts: []
+            courses: [],
+            challenges: []
         }
     }
 
     componentDidMount(){
-        console.log("Yes")
+        axios.get("https://algoviz-pyflask-rest-api.herokuapp.com/api/v1/courses/")
+        .then(response=>{
+            console.log(response)
+            this.setState({courses:response.data})
+        })
+        .catch(error=>{console.log(error)})    
+
+        axios.get("https://algoviz-pyflask-rest-api.herokuapp.com/api/v1/challenges/")
+        .then(response=>{
+            console.log(response)
+            this.setState({challenges:response.data})
+        })
+        .catch(error=>{console.log(error)})    
     }   
 
     render(){
+        const {courses} =this.state
+        const {challenges} = this.state
         return (
             <div className="userProfile">
                 <div className="left-content column">
@@ -25,8 +41,22 @@ class UserProfile extends Component {
                 </div>
                 <div className="right-content column">
                     <div className="spacer"></div>
-                    <h4>Courses Completed</h4>
-                    <h4>Challenges Completed</h4>
+                    <h4>Courses</h4>
+                    <ol>
+                        {
+                            courses.length ?
+                            courses.map(course => <li key ={course.id}>{course.title}: {course.description} </li>):
+                            null
+                        }
+                    </ol>
+                    <h4>Challenges</h4>
+                    <ol>
+                        {
+                            challenges.length ?
+                            challenges.map(challenge => <li key ={challenge.id}>{challenge.title} </li>):
+                            null
+                        }
+                    </ol>
                 </div>
             </div>
         )
