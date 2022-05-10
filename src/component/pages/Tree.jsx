178e099e160be570a2d0aac4516e9e00e753465d
James Yoo <james.yoo715@gmail.com>
2022-05-10 14:30:29 -0400

    set up binary tree

--- src/component/pages/Tree.jsx
+++ src/component/pages/Tree.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import BinaryTree from '../visualizer/LearnBinaryTree';
 
-const Treepage = (props) => {
- return (
+class Treepage extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
     <div className='shell'>
       <header className='shell-header'>
           <h1>Binary Trees</h1>
       </header>
       <main className='shell-body'>
+        <BinaryTree ref={BinaryTree=>this.BinaryTree = BinaryTree}></BinaryTree>
       </main>
       <main className='shell-bodyII'>
+        <h2>What is a Binary Tree</h2>
+        <p></p>
+        <hr></hr>
+        <h2>Add method</h2>
+        <p>Inserts a value into the tree at the first open spot from the first node missing a left or right child</p>
+        <hr></hr>
       </main>
       <footer className='shell-footer'>
       </footer>
     </div>
- );
-};
- 
+  );
+  }
+}
+
 export default Treepage;
