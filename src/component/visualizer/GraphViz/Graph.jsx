ad85905debe7afb16408cbc8aa0493eb48ce5363
Azaan <xxx.azaan.ali@gmail.com>
2022-05-20 07:13:43 -0400

    Directed and Weighted Graphs

--- src/component/visualizer/GraphViz/Graph.jsx
+++ src/component/visualizer/GraphViz/Graph.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useReducer } from "react";
 import Draggable from "react-draggable";
 import Xarrow from "react-xarrows";
-import { BFS } from "./algorithms/GraphAlgo";
+import { BFS, Dijkstra } from "./algorithms/GraphAlgo";
 import "./Graph.css";
 import {
   Button,
@@ -15,44 +15,42 @@ import {
   DialogContentText,
   DialogTitle,
   TextField,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
-
-const Circle = (x, y) => {
-  return {
-    padding: 0,
-    margin: 5,
-    width: 50,
-    height: 50,
-    backgroundColor: "white",
-    color: "black",
-    borderRadius: "50%",
-    border: "1px solid black",
-    lineHeight: "50px",
-    outlineWidth: 0,
-    left: `${x - 25}px`,
-    top: `${y - 25}px`,
-  };
-};
 
 const GraphViz = () => {
   const [adjList, setAdjList] = useState(new Map());
   const [adjListHTML, setAdjListHTML] = useState(new Map());
+  const [adjListAlgo, setAdjListAlgo] = useState(new Map());
   const [size, setSize] = useState(0);
   const [path, setPath] = useState([]);
   const [openEdges, setopenEdges] = useState(false);
   const [sourceV, setSourceV] = useState(0);
   const [destV, setDestV] = useState(0);
-  const [unweighted, setWeighted] = useState("unweighted");
-  const [undirected, setDirected] = useState("undirected");
+  const [weighted, setWeighted] = useState(false);
+  const [directed, setDirected] = useState(false);
   const [algoOption, setAlgoOption] = useState("Breath First Search");
   const [node1, setNode1] = useState(0);
   const [node2, setNode2] = useState(0);
+  const [edgeWeight, setEdgeWeight] = useState(1);
+  const [edgeDirection, setEdgeDirection] = useState("->");
 
   useEffect(() => {
     createGraph();
   }, []);
 
   const [_, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  const handleChangeW = (e) => {
+    setWeighted(e.target.checked);
+    createGraph();
+  };
+
+  const handleChangeD = (e) => {
+    setDirected(e.target.checked);
+    createGraph();
+  };
 
   const handleAlgoSelect = (e) => {
     setAlgoOption(e.target.value);
@@ -66,12 +64,15 @@ const GraphViz = () => {
     setopenEdges(false);
     setNode1(0);
     setNode2(0);
+    setEdgeWeight(1);
+    setEdgeDirection("->");
   };
 
   const createGraph = () => {
     setSize(0);
     setAdjList(new Map());
     setAdjListHTML(new Map());
+    setAdjListAlgo(new Map());
     setPath([]);
   };
 
@@ -84,6 +85,7 @@ const GraphViz = () => {
   const addVertex = (e, i) => {
     if (!adjList.has(i)) {
       adjList.set(i, []);
+      adjListAlgo.set(i, []);
       let [x, y] = getCoordinates(e);
       adjListHTML.set(i, () => (
         <Draggable
@@ -92,22 +94,81 @@ const GraphViz = () => {
           onStop={forceUpdate}
           onDrag={forceUpdate}
         >
-          <div
-            className={`vertex vertex-${i}`}
-            id={`vertex-${i}`}
-            style={Circle(x, y)}
-          >
-            {i}
+          <div style={{ width: "50px", height: "50px" }}>
+            <div
+              id={`weight-${i}`}
+              style={{ width: "50px", height: "25px", color: "blue" }}
+            >
+              Infinity
+            </div>
+            <div
+              className={`vertex vertex-${i}`}
+              style={{
+                width: "50",
+                height: "50",
+                backgroundColor: "white",
+                color: "black",
+                borderRadius: "50%",
+                border: "1px solid black",
+                lineHeight: "50px",
+              }}
+              id={`vertex-${i}`}
+            >
+              {i}
+            </div>
           </div>
         </Draggable>
       ));
     }
   };
 
+  function Edge(node, weight, direction) {
+    this.node = node;
+    this.weight = weight;
+    this.direction = direction;
+  }
+
+  function WeightedEdge(node, weight) {
+    this.node = node;
+    this.weight = weight;
+  }
+
   const addEdge = () => {
     if (node1 != node2 && adjList.has(node1) && adjList.has(node2)) {
-      if (!adjList.get(node1).includes(node2))
-        adjList.set(node1, [...adjList.get(node1), node2]);
+      if (!weighted && !directed) {
+        if (!adjList.get(node1).includes(node2)) {
+          adjList.set(node1, [...adjList.get(node1), node2]);
+          adjListAlgo.set(node1, [...adjListAlgo.get(node1), node2]);
+          adjListAlgo.set(node2, [...adjListAlgo.get(node2), node1]);
+        }
+      } else if ((directed && weighted) || (directed && !weighted)) {
+        if (!weighted) setEdgeWeight(1);
+        let e = new Edge(node2, edgeWeight, edgeDirection);
+        if (!adjList.get(node1).includes(e)) {
+          adjList.set(node1, [...adjList.get(node1), e]);
+          if (edgeDirection == "->") {
+            let ew = new WeightedEdge(node2, edgeWeight);
+            adjListAlgo.set(node1, [...adjListAlgo.get(node1), ew]);
+          } else if (edgeDirection == "<->") {
+            let ew = new WeightedEdge(node2, edgeWeight);
+            adjListAlgo.set(node1, [...adjListAlgo.get(node1), ew]);
+            ew = new WeightedEdge(node1, edgeWeight);
+            adjListAlgo.set(node2, [...adjListAlgo.get(node2), ew]);
+          } else if (edgeDirection == "<-") {
+            let ew = new WeightedEdge(node1, edgeWeight);
+            adjListAlgo.set(node2, [...adjListAlgo.get(node2), ew]);
+          }
+        }
+      } else if (!directed && weighted) {
+        let e = new Edge(node2, edgeWeight, null);
+        let ew = new WeightedEdge(node2, edgeWeight);
+        let ew2 = new WeightedEdge(node1, edgeWeight);
+        if (!adjList.get(node1).includes(e)) {
+          adjList.set(node1, [...adjList.get(node1), e]);
+          adjListAlgo.set(node1, [...adjListAlgo.get(node1), ew]);
+          adjListAlgo.set(node2, [...adjListAlgo.get(node2), ew2]);
+        }
+      }
     }
     handleEdgesClose();
   };
@@ -129,40 +190,95 @@ const GraphViz = () => {
       let arr = adjList.get(i);
       if (arr.length === 0) continue;
       for (let j = 0; j < arr.length; j++) {
-        edges.push(
-          <Xarrow
-            key={`edge${i}-${arr[j]}`}
-            id={`${i}-${arr[j]}`}
-            start={`vertex-${i}`}
-            end={`vertex-${arr[j]}`}
-            color="black"
-            showHead={false}
-          ></Xarrow>
-        );
+        if (!weighted && !directed) {
+          edges.push(
+            <Xarrow
+              key={`edge${i}-${arr[j]}`}
+              id={`${i}-${arr[j]}`}
+              start={`vertex-${i}`}
+              end={`vertex-${arr[j]}`}
+              color="black"
+              showHead={false}
+            ></Xarrow>
+          );
+        } else if (directed || weighted) {
+          let x = arr[j].direction == "<->";
+          let w = arr[j].weight;
+          if (directed && !weighted) {
+            w = "";
+          }
+          edges.push(
+            <Xarrow
+              key={`edge${i}-${arr[j].node}`}
+              id={`${i}-${arr[j].node}`}
+              start={`vertex-${i}`}
+              end={`vertex-${arr[j].node}`}
+              color="black"
+              labels={<h5 style={{ color: "red" }}>{w}</h5>}
+              showHead={x || arr[j].direction == "->"}
+              showTail={x || arr[j].direction == "<-"}
+            ></Xarrow>
+          );
+        }
       }
     }
     return edges;
   };
 
   const visualize = () => {
-    let visited = null;
     switch (algoOption) {
       case "Breath First Search":
-        visited = BFS(sourceV, adjList);
+        let visited = BFS(sourceV, destV, adjListAlgo, weighted, directed);
+        if (visited == "No Path Found") alert("No Path Found");
+        else visualizePath(visited);
+        break;
+      case "Dijkstra":
+        let [path, distances, visit] = Dijkstra(
+          sourceV,
+          destV,
+          adjListAlgo,
+          weighted,
+          directed
+        );
+        if (path == "No Path Found") alert("No Path Found");
+        else visualizeDijkstra(path, distances, visit);
         break;
     }
-    visualizePath(visited);
+  };
+
+  const visualizeDijkstra = (path, distances, visited) => {
+    let str = "";
+    for (let i = 0; i < path.length; i++) {
+      str += path[i] + ", ";
+    }
+    for (let i = 0; i < visited.length; i++) {
+      setTimeout(() => {
+        document.getElementById(
+          `vertex-${visited[i][0]}`
+        ).style.backgroundColor = "red";
+        document.getElementById(`weight-${visited[i][0]}`).textContent =
+          visited[i][1];
+      }, 1000 * i);
+      setTimeout(() => {
+        document.getElementById(
+          `vertex-${visited[i][0]}`
+        ).style.backgroundColor = "white";
+      }, 1000 * i + 1000);
+    }
+    setPath(str);
   };
 
   const visualizePath = (p) => {
     let str = "";
-    for (let i of p) {
+    for (let i = 0; i < p.length; i++) {
       setTimeout(() => {
-        document.getElementById(`vertex-${i}`).style.borderColor = "red";
-        str += i.toString() + ", ";
+        let e = document.getElementById(`vertex-${p[i]}`).style;
+        e.backgroundColor = "red";
+        str += p[i].toString() + ", ";
       }, 1000 * i);
       setTimeout(() => {
-        document.getElementById(`vertex-${i}`).style.borderColor = "black";
+        document.getElementById(`vertex-${p[i]}`).style.backgroundColor =
+          "white";
         setPath(str);
       }, 1000 * i + 1000);
     }
@@ -185,6 +301,14 @@ const GraphViz = () => {
         <Button variant="outlined" onClick={handleEdgesOpen}>
           Create Edge
         </Button>
+        <FormControlLabel
+          control={<Checkbox checked={directed} onChange={handleChangeD} />}
+          label="Directed"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={weighted} onChange={handleChangeW} />}
+          label="Weighted"
+        />
         <Dialog open={openEdges} onClose={handleEdgesClose}>
           <DialogTitle>Create Edges</DialogTitle>
           <DialogContent>
@@ -213,6 +337,33 @@ const GraphViz = () => {
                 setNode2(Number(e.target.value));
               }}
             />
+            <TextField
+              autoFocus
+              align="center"
+              id="outlined-number"
+              label="Edge Weight"
+              type="number"
+              variant="standard"
+              onChange={(e) => {
+                setEdgeWeight(Number(e.target.value));
+              }}
+            />
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id="select-label">Direction</InputLabel>
+              <Select
+                labelId="select-label"
+                id="simple-select"
+                value={edgeDirection}
+                label="Direction"
+                onChange={(e) => {
+                  setEdgeDirection(e.target.value);
+                }}
+              >
+                <MenuItem value={"->"}>{"->"}</MenuItem>
+                <MenuItem value={"<->"}>{"<->"}</MenuItem>
+                <MenuItem value={"<-"}>{"<-"}</MenuItem>
+              </Select>
+            </FormControl>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleEdgesClose}>Cancel</Button>
@@ -250,6 +401,7 @@ const GraphViz = () => {
             <MenuItem value={"Breath First Search"}>
               Breath First Search
             </MenuItem>
+            <MenuItem value={"Dijkstra"}>Dijkstra</MenuItem>
           </Select>
         </FormControl>
         <Button color="primary" variant="outlined" onClick={visualize}>
--- src/component/visualizer/GraphViz/algorithms/GraphAlgo.js
+++ src/component/visualizer/GraphViz/algorithms/GraphAlgo.js
@@ -1,21 +1,143 @@
-function BFS(s, adjList) 
-{
-   let visited = new Set()
-   const q = [s]
-   visited.add(s)
-   while (q.length > 0) {
-    let temp = q.shift();
-    adjList.get(temp).filter(i => !visited.has(i)).forEach(i => {
-        visited.add(i)
-        q.push(i)
-    })
-   }
-   return visited
+import Queue from "../../../DSA/DataStructures/Queue/queue";
+
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+  enqueue(node, priority) {
+    let e = { node, priority };
+    let contain = false;
+
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].priority > e.priority) {
+        this.items.splice(i, 0, e);
+        contain = true;
+        break;
+      }
+    }
+    if (!contain) {
+      this.items.push(e);
+    }
+  }
+  dequeue() {
+    if (this.isEmpty()) return;
+    return this.items.shift();
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
 }
 
-function Dijkstra(s, d, adjList) 
-{
+function BFS(s, des, adjList, w, d) {
+  let visited = [];
+  let q = new Queue();
+  q.enqueue(s);
+  visited.push(s);
+  if ((!w && !d)) {
+    while (!q.isEmpty()) {
+      let v = q.dequeue();
+      let neighbors = adjList.get(v);
+      for (let i = 0; i < neighbors.length; i++) {
+        if (!visited.includes(neighbors[i])) {
+          visited.push(neighbors[i]);
+          q.enqueue(neighbors[i]);
+          if (neighbors[i] == des) {
+            console.log(visited);
+            return visited;
+          }
+        }
+      }
+    }
+  } else {
+    while (!q.isEmpty()) {
+      let v = q.dequeue();
+      let neighbors = adjList.get(v);
+      for (let i = 0; i < neighbors.length; i++) {
+        if (!visited.includes(neighbors[i].node)) {
+          visited.push(neighbors[i].node);
+          q.enqueue(neighbors[i].node);
+          if (neighbors[i].node == des) {
+            return visited;
+          }
+        }
+      }
+    }
+  }
 
+  return "No Path Found";
+}
+
+function Dijkstra(s, des, adjList, w, d) {
+  let animations = [];
+  if ((!w && d) || (w && d) || (d && !w)) {
+    if (BFS(s, des, adjList, w, d) == "No Path Found") {
+      return ["No Path Found", null, null];
+    }
+    let distances = {};
+    let prev = {};
+    let pq = new PriorityQueue();
+    for (let i = 0; i < adjList.size; i++) {
+      distances[i] = Infinity;
+      prev[i] = null;
+    }
+    distances[s] = 0;
+    animations.push([s, 0]);
+    pq.enqueue(s, 0);
+    while (!pq.isEmpty()) {
+      let x = pq.dequeue();
+      let node = x.node;
+      let neighbors = adjList.get(node);
+      for (let i = 0; i < neighbors.length; i++) {
+        let temp = distances[node] + neighbors[i].weight;
+        if (temp < distances[neighbors[i].node]) {
+          distances[neighbors[i].node] = temp;
+          animations.push([neighbors[i].node, temp]);
+          prev[neighbors[i].node] = node;
+          pq.enqueue(neighbors[i].node, temp);
+        }
+      }
+    }
+    let path = [des];
+    let itr = des;
+    while (itr != s) {
+      itr = prev[itr];
+      path.push(itr)
+    }
+    return [path, distances, animations];
+  } else {
+    if (BFS(s, des, adjList, w, d) == "No Path Found") {
+      return ["No Path Found", null, null];
+    }
+    let distances = {};
+    let prev = {};
+    let pq = new PriorityQueue();
+    for (let i = 0; i < adjList.size; i++) {
+      distances[i] = Infinity;
+      prev[i] = null;
+    }
+    distances[s] = 0;
+    pq.enqueue(s, 0);
+    while (!pq.isEmpty()) {
+      let x = pq.dequeue();
+      let node = x.node;
+      let neighbors = adjList.get(node);
+      for (let i = 0; i < neighbors.length; i++) {
+        let temp = distances[node] + 1;
+        if (temp < distances[neighbors[i]]) {
+          distances[neighbors[i]] = temp;
+          prev[neighbors[i]] = node;
+          pq.enqueue(neighbors[i], temp);
+        }
+      }
+    }
+    let path = [des];
+    let itr = des;
+    while (itr != s) {
+      itr = prev[itr];
+      path.push(itr)
+    }
+    return [path, distances, "test"];
+  }
 }
 
 export { BFS, Dijkstra };
